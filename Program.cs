5ded521a0b165bab31e08c216cca62e4f132f7b6
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-15 22:34:45 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Maze test = new Maze();
+
+Console.WriteLine(test.Size);
+
+public struct Maze
+{
+    // Parameterless constructor with property initialization 
+    public Maze()
+    {
+        Size = 10;
+    }
+
+    // Initialization of the property at its declaration
+    public int Size { get; set; } = 15;
+}
